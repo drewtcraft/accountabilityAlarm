9fa8f4d309e5eb46833a9fe16ec2c8260a3626ea
andrew <drewtcraft@gmail.com>
2018-05-28 19:49:01 -0400

    added scheduling functionality

--- App/App.js
+++ App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, AsyncStorage} from 'react-native';
+import { StyleSheet, Text, View, Button, AsyncStorage, DatePickerIOS} from 'react-native';
 const helper = require('./helper.js')
 
 
@@ -7,14 +7,33 @@ const helper = require('./helper.js')
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { chosenDate: new Date() };
 
-
-    this.sendInfo = this.sendInfo.bind(this)
+    this.setDate = this.setDate.bind(this);
     this.sendMessage = this.sendMessage.bind(this)
     this.getUser = this.getUser.bind(this)
     this.saveUser = this.saveUser.bind(this)
-    this.websocketFunk = this.websocketFunk.bind(this)
+    this.setAlarm = this.setAlarm.bind(this)
   }
+
+  setAlarm(){
+    console.log('button working')
+    this.sendMessage('setAlarm', this.state.chosenDate)
+  }
+
+  setDate(newDate) {
+    //parse new date from nasty weird object
+    const year = JSON.stringify(newDate).slice(1, 5)
+    const month = `${parseInt(JSON.stringify(newDate).slice(6, 8))-1}`
+    const day = JSON.stringify(newDate).slice(9, 11)
+    const hourPlusFive = JSON.stringify(newDate).slice(12, 14)
+    const hour = `${parseInt(hourPlusFive)-4}`
+    const minute = JSON.stringify(newDate).slice(15, 17)
+
+    const stringDate = `${year} ${month} ${day} ${hour} ${minute}`
+    const setDate = new Date(year, month, day, hour, minute, 0)
+    this.setState({chosenDate: setDate})
+ }
 
   componentWillMount(){
     this.getUser().then((data)=> {
@@ -26,80 +45,55 @@ export default class App extends React.Component {
 
   componentDidMount() {
 
-    ws = new WebSocket('ws://accountability-alarm.herokuapp.com/:5000');
+    //ws = new WebSocket('ws://accountability-alarm.herokuapp.com/:5000');
+    ws = new WebSocket('ws://192.168.1.138:5001')
+
     ws.onopen = () => {
       console.log('cnxn est')
 
+      //if there is no user (found in cWM), send 'no_user'
       if(this.state.user !== 'no_user' || this.state.user !== null ){
         console.log('user:', this.state.user)
-        this.sendMessage(this.state.user)
+        this.sendMessage('user', this.state.user)
       } else {
         console.log('no user detected, generating new id')
-        this.sendMessage('no_user')
+        this.sendMessage('user', 'no_user')
       }
 
     }
 
-
-      ws.onmessage = (message) => {
-        console.log(message.data)
-      //
+    ws.onmessage = (message) => {
+      console.log('current message', message.data)
     }
-      this.websocketFunk()
+  }
 
-    }
 
-    websocketFunk(){
-      ws = new WebSocket('ws://accountability-alarm.herokuapp.com/:5000');
-      ws.onopen = () => {
-        console.log('cnxn est')
-
-        if(this.state.user !== 'no_user'){
-          console.log('user:', this.state.user)
-          this.sendMessage(this.state.user)
-        } else {
-          console.log('no user detected, generating new id')
-          this.sendMessage('no_user')
-        }
-
+  async getUser(){
+    try {
+      const value = await AsyncStorage.getItem('@MyStore:user');
+      if (value !== null){
+        console.log('value', value)
+        return value
       }
-
-
-        ws.onmessage = (message) => {
-          console.log(message.data)
-        //
-      }
+      return 'no_user'
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    async getUser(){
-      try {
-        const value = await AsyncStorage.getItem('@MyStore:user');
-        if (value !== null){
-          console.log('value', value)
-          return value
-        }
-        return 'no_user'
-      } catch (error) {
-        console.log(error)
-        return 'no_user'
-      }
+  async saveUser(user_id){
+    try {
+      await AsyncStorage.setItem('@MyStore:user', user_id);
+      console.log('suxess')
+    } catch (error) {
+      console.log(user_id, 'not saved', error)
     }
+  }
 
-    async saveUser(user_id){
-      try {
-        await AsyncStorage.setItem('@MyStore:user', user_id);
-        console.log('suxess')
-      } catch (error) {
-        console.log(user_id, 'not saved', error)
-      }
-    }
-
-    sendMessage(payload){
-      ws.send(JSON.stringify({message: payload}))
-    }
-
-  sendInfo(){
-    this.ws.send(JSON.stringify({message: 'hey'}))
+  sendMessage(type, payload){
+    let obj = {}
+    obj[type] = payload
+    ws.send(JSON.stringify({message: obj}))
   }
 
 
@@ -107,12 +101,15 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button title='shit' onPress={()=>{this.websocketFunk()
-          console.log('shjti')
+        <Button title='set alarm' onPress={()=>{
+          this.setAlarm()
         }} />
-        <Text >Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+
+        <DatePickerIOS
+          date={this.state.chosenDate}
+          onDateChange={this.setDate}
+        />
+
       </View>
     );
   }
@@ -122,7 +119,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
 });
--- server.js
+++ server.js
@@ -4,45 +4,129 @@ let express = require('express')
 let schedule = require('node-schedule')
 let app = express()
 let MongoClient = require('mongodb').MongoClient
+let assert = require('assert')
 let port = process.env.PORT || 5001
 
+
+
+
+//configure http server
 app.use(express.static(__dirname + "/"))
 
 let server = http.createServer(app)
 server.listen(port)
+console.log("http server listening on %d", port)
 
 
-function scheduleAlarm(){
-  
+
+
+
+
+//this will probably end up being the main function
+//on setting the alarm, it must store some kind of id so the user can cancel it later
+function scheduleAlarm(date, user1, user2){
+  //create 'room' with both users and commit to db
+  //also send room information to clients?
+  //maybe have to store room for each key (username)
+
+
+  const alarmName = schedule.scheduleJob(date, ()=>{
+    //send signal to raspberry pi
+    //initiate some sort of game mode (send game information to client on connection)
+    //wait for next message from client, maybe store instructions for each client in DB
+
+
+  })
+
+  return alarmName
+
 }
+
+
+
+
 
 // Connect to the db -- potentially move entire db into ws.on('message')
 MongoClient.connect("mongodb://andrew:sevens7@ds235840.mlab.com:35840/heroku_rbffwg9m", function (err, db) {
 
     dbo = db.db('heroku_rbffwg9m')
-    let test = {hey: 'bro', its: 'me'}
-    dbo.collection("collection").insertOne(test, function(err, res) {
+    // let test = {hey: 'bro', its: 'me'}
+    // dbo.collection("collection").insertOne(test, function(err, res) {
     if (err) throw err;
-    console.log("1 document inserted");
     db.close();
-  });
+  // });
 
 });
 
-console.log("http server listening on %d", port)
+function handleUser(user_id){
+  if(user_id === 'no_user'){
+    let uniqueID = `${new Date}${Math.random()*10000000}`
+    ws.send()
+    console.log(uniqueID)
+  }
+}
 
+
+
+
+//configure websocket server
 let wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
+let user = 'no_user'
+
 wss.on("connection", function(ws) {
+
+  ws.on('message', function incoming(message) {
+    const messageBody = JSON.parse(message).message
+    const messageType = Object.keys(messageBody)[0]
+
+    console.log('incoming message type:', messageType)
+
+    switch(messageType){
+      case 'user':
+        user = JSON.parse(message).message['user']
+        break;
+      case 'setAlarm':
+        console.log(JSON.parse(message).message['setAlarm'])
+        schedule.scheduleJob(JSON.parse(message).message['setAlarm'], ()=>{
+          console.log('this is a test alarm')
+        })
+        break;
+      case 'deleteAlarm':
+        console.log(JSON.parse(message).message['deleteAlarm'])
+        break;
+      case 'gameState':
+        console.log(JSON.parse(message).message['gameState'])
+        break;
+      default:
+        break;
+    }
+
+    //check id
+      //does user have stored id? if not generate, store, and send
+      //or if server cannot locate the id, generate new id, store, and send
+      //or it it is valid, check the instructions
+
+
+    //if alarm message...
+      //const alarm = 0
+      //find another user with alarm - if none exists, store the alarm and wait
+      //if you find another user with the same alarm, schedule new alarm
+      //let newAlarm = scheduleAlarm(alarm, user)
+      //save newAlarm to db AND send back to user, like so...
+      //save
+      //ws.send(newAlarm)
+
+    //if alarm cancel...
+      //set alarm id to variable
+      //cancel alarm
+
+  })
+
 
   ws.send('you got it bitch')
 
   console.log("websocket connection open")
 
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
 })
